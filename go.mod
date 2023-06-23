075479e62bb34ed637ff73935f27cc9e74b9ac97
diego cabulo <diego.cabulo@linkti.co>
2023-06-22 23:53:54 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/dcabulo/go_movies_api
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "43827", Title: "Movie One", Director: &Director{Firstname: "Diego", Lastname: "cabulo"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "43828", Title: "Movie Two", Director: &Director{Firstname: "Ivan", Lastname: "cabulo"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Print("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
